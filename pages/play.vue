e7299e2a0af24059c731ce5e4d5e96439ad51013
Sota Giorgadze <giorgadze@inbox.lv>
2023-10-06 20:42:29 +0200

    check available lifecycle methods and play with @click directive

--- pages/play.vue
+++ pages/play.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="container bg-red-500">
+  <div
+    class="container mx-auto bg-red-500"
+    @click.capture="isOpen ? closeDropdown() : ''"
+  >
     <div class="relative">
       <!-- Trigger button -->
-      <button class="text-white focus:outline-none" @click="toggleDropdown(33)">
+      <button
+        class="text-white focus:outline-none"
+        @click.passive="toggleDropdown(33)"
+      >
         Click me
       </button>
 
@@ -12,10 +18,7 @@
         :class="isOpen ? 'opacity-1 scale-1' : 'scale-0 opacity-0'"
       >
         <!-- Dropdown items -->
-        <div
-          class="bg-slate-200 py-2 dark:bg-slate-600"
-          @click="closeDropdown()"
-        >
+        <div class="bg-slate-200 py-2 dark:bg-slate-600">
           <!-- Your dropdown items here -->
           <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200"
             >Item 1</a
@@ -38,8 +41,23 @@ const isOpen = ref(false);
 
 const toggleDropdown = (num) => {
   isOpen.value = !isOpen.value;
-  console.log(num);
+  // console.log(num);
 };
+onBeforeMount(() => {
+  console.log("I am the first one");
+});
+onMounted(() => {
+  console.log("I am the second one");
+});
+onBeforeUpdate(() => {
+  console.log("I am the third one");
+});
+onUpdated(() => {
+  console.log("I am the fourth one");
+});
+onBeforeUnmount(() => {
+  console.log("I am the fifth one");
+});
 
 const closeDropdown = () => {
   isOpen.value = false;
